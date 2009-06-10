0e25ab7dbd68105287519265e41f645dda1cf093
Simon Roe <simon.roe@talusdesign.co.uk>
2009-06-10 10:55:01 +0100

    changed password

--- web/settings.py
+++ web/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'fishsubsidy_fishjango'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_USER = 'fishsubsidy'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'fishsubsidy'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
