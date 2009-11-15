8571ee8399c2621067ad50dcdea457b22839800e
Simon Roe <simon.roe@talusdesign.co.uk>
2009-11-15 21:18:54 +0000

    update from live site

--- web/feeds/management/commands/parse_feeds.py
+++ web/feeds/management/commands/parse_feeds.py
@@ -1,8 +1,10 @@
-import django
-from django.core.management.base import NoArgsCommand, CommandError
 import sys
 sys.path.append('../../../')
+sys.path.append('/var/www/fishsubsidy/web')
 from feeds import parse
+
+import django
+from django.core.management.base import NoArgsCommand, CommandError
 
 class Command(NoArgsCommand):
   def handle_noargs(self, **options):
--- web/settings.py
+++ web/settings.py
@@ -1,10 +1,10 @@
 # Django settings for web project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+   # ('Sym Fish', 'fishsubsidy@talusdesign.co.uk'),
 )
 
 MANAGERS = ADMINS
