e908a69617b1d127cc14494741b9b3a2a8b3effa
Simon Roe <simon.roe@talusdesign.co.uk>
2010-01-07 16:13:33 +0000

    Fix API key for google maps

--- web/data/templatetags/map.py
+++ web/data/templatetags/map.py
@@ -1,6 +1,8 @@
 from django.template import Library, Node
 register = Library()
 
+import settings
+
 def get_map(lat,lon):
   try:
     t = float(lat) + 0.03
@@ -10,5 +12,6 @@ def get_map(lat,lon):
     l = float(lon) - 0.03
   except:
     fail = True
+  api_key = settings.GOOGLE_MAPS_API_KEY
   return locals()
 register.inclusion_tag('blocks/map.html')(get_map)
