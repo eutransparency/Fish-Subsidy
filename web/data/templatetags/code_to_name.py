19d40b2d3aa215769a2b98ffe10f8c5c12e5cecf
Sym Roe <simon.roe@talusdesign.co.uk>
2010-04-14 10:33:08 +0100

    More translation strings

--- web/data/templatetags/code_to_name.py
+++ web/data/templatetags/code_to_name.py
@@ -1,9 +1,23 @@
 from django.template import Library, Node
+from django.template import Context, Variable
 from misc import countryCodes
 register = Library()
 
-def code_to_name(code, locale=None):
-  if code == "UK":
-    code = "GB"
-  return countryCodes.country_codes(code)['name']
-register.simple_tag(code_to_name)
+class CodeToName(Node):
+    def __init__(self, code, varname):
+        self.code = code            
+        self.varname = varname
+    
+    def render(self, context):
+        self.code = Variable(self.code).resolve(context)
+        if self.code == "UK":
+            self.code = "GB"
+
+        context[self.varname] = countryCodes.country_codes(self.code)['name']
+        return ''
+
+@register.tag
+def code_to_name(parser, token):
+    bits = token.contents.split()
+    print bits
+    return CodeToName(bits[1], bits[3])
