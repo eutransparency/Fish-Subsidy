d6f5ee31294498500b1f255cd16426b8c065c74c
Simon Roe <simon.roe@talusdesign.co.uk>
2010-04-28 14:50:42 +0100

    default from email

--- web/global_settings.py
+++ web/global_settings.py
@@ -130,10 +130,7 @@ SESSION_TIMEOUT = 1209600
 REGISTRATION_BACKEND = "registration.backends.default.DefaultBackend"
 ACCOUNT_ACTIVATION_DAYS = 15
 
-<<<<<<< HEAD:web/global_settings.py
 DEFAULT_FROM_EMAIL = "team@fishsubsidy.org"
-=======
 
 DJAPIAN_DATABASE_PATH = "xapian.db"
 DJAPIAN_STEMMING_LANG = "multi"
->>>>>>> e4c7984d0cb3a2d00fb7e72edbde98476e64e6cd:web/global_settings.py
