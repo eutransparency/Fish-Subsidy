ca835679a9847efdceeae32ef09384cdbe9455b5
Sym Roe <sym.roe@talusdesign.co.uk>
2011-04-19 09:03:28 +0100

    tests

--- web/data/tests/__init__.py
+++ web/data/tests/__init__.py
@@ -1,3 +1,4 @@
 # from data.tests.test_schemes import *
-from data.tests.test_views import *
-from data.tests.test_misc import *
+# from data.tests.test_views import *
+# from data.tests.test_misc import *
+from data.tests.test_models import *
