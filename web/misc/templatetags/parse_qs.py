9386c2b73768183fd499caf7631e3720dd48ab66
Sym Roe <sym.roe@talusdesign.co.uk>
2011-10-12 01:01:10 +0100

    don't fail if no sort query string isn't given

--- web/misc/templatetags/parse_qs.py
+++ web/misc/templatetags/parse_qs.py
@@ -1,5 +1,4 @@
-from django.template import Library, Node
-import urlparse
+from django.template import Library
 import urllib
 register = Library()
 
@@ -17,15 +16,16 @@ def parse_qs(qs, k=None,v=None):
 def parse_sort_qs(qs, k=None, v=None):
     qs = qs.copy()
     old_v = qs.get(k)
-    if k:
-        if v in [old_v, old_v[1:]]:
-            # We're updating the same key, and just reversing the order
-            if old_v.startswith('-'):
-                v = old_v[1:]
-            else:
-                v = "-%s" % old_v
+    if old_v:
+        if k:
+            if v in [old_v, old_v[1:]]:
+                # We're updating the same key, and just reversing the order
+                if old_v.startswith('-'):
+                    v = old_v[1:]
+                else:
+                    v = "-%s" % old_v
 
-        qs[k] = v
+            qs[k] = v
 
 
     # Normally, we don't want to keep the page element of the string
