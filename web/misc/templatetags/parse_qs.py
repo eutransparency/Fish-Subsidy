3ba2668c1210a4f009fd44ac658b8f0842aa52e5
Sym Roe <sym.roe@talusdesign.co.uk>
2011-10-19 16:12:40 +0100

    Make sort and parse_ps work properly

--- web/misc/templatetags/parse_qs.py
+++ web/misc/templatetags/parse_qs.py
@@ -7,7 +7,6 @@ def parse_qs(qs, k=None,v=None):
     qs = qs.copy()
     get_dict = {}
 
-
     get_dict[k] = unicode(v).encode('utf8')
 
     for old_k, old_v in qs.items():
@@ -24,8 +23,9 @@ def parse_qs(qs, k=None,v=None):
 @register.simple_tag
 def parse_sort_qs(qs, k=None, v=None):
     qs = qs.copy()
-    for k,v in qs.items():
-        qs[k] = unicode(v).encode('utf8')
+    get_dict = {}
+    for old_k,old_v in qs.items():
+        get_dict[old_k] = unicode(old_v).encode('utf8')
 
     old_v = qs.get(k, "")
     if k:
@@ -35,12 +35,18 @@ def parse_sort_qs(qs, k=None, v=None):
                 v = old_v[1:]
             else:
                 v = "-%s" % old_v
-
-        qs[k] = v
-
+        # else:
+    get_dict[k] = v
+    print get_dict
 
     # Normally, we don't want to keep the page element of the string
-    if 'page' in qs:
-        del qs['page']
-    return "?%s" % urllib.urlencode(dict(qs))
+    if 'page' in get_dict:
+        del get_dict['page']
+    return "?%s" % urllib.urlencode(get_dict)
+
+
+
+
+
+
 
