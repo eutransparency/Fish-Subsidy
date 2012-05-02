2c2ef331809b6f301e2cd535267caebea44e117b
Sym Roe <sym.roe@talusdesign.co.uk>
2012-05-02 16:52:44 +0100

    Refactor features context_processor to make it more clear

--- web/features/context_processors.py
+++ web/features/context_processors.py
@@ -10,14 +10,16 @@ def featured_items(request):
     features = {}
     
     if in_features:
-        features['featured_items'] = cache.get('featured_items')
-        
-        if not features['featured_items']:
-            features['featured_items'] = Feature.objects.filter(featured=True, published=True,).order_by('-pk')
-            cache.set('featured_items', featured_items)
-        else:
-            return {'featured_items' : featured_items}
-    else:
+        # Don't show context features on features page!
         return {}
+
+    features['featured_items'] = cache.get('featured_items')
+        
+    if not features['featured_items']:
+        features['featured_items'] = Feature.objects.filter(featured=True, published=True,).order_by('-pk')
+        cache.set('featured_items', features['featured_items'])
+
+    return {'featured_items' : features['featured_items']}
+
         
     
